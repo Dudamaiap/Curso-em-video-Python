aff0af4e1ff23036ad42fcf3849b035d8416e66b
Maria Eduarda Maia Pereira <dudamaiapereira@gmail.com>
2024-06-26 17:20:07 -0400

    ex003 e 004

---
+++ ex003.py
@@ -0,0 +1,7 @@
+'''Crie um programa que leia dois números e mostre a soma entre eles'''
+
+n1=int(input('Digite o primeiro numero:'))
+n2=int(input('Digite o segundo numero:'))
+s=n1+n2
+print('A soma do numero {} e {} é {}'.format(n1,n2,s))
+
---
+++ ex004.py
@@ -0,0 +1,6 @@
+'''Faça um programa que leia algo e mostre na tela o seu tipo primitivo e todas as informações
+possíveis sobre ele'''
+
+entrada=input('Digite algo:')
+print('O tipo primitivo desse valor é', type(entrada))
+
